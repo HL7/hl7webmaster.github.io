d2d307128b0d2c528635e5c9626430b8dca5a507
jmazella <jmazella@mitre.org>
2022-10-31 16:19:08 -0400

    Adding contact form and emailjs

--- js/scripts.js
+++ js/scripts.js
@@ -9,3 +9,16 @@ function closemenu(){
 	document.querySelector('#open-menu-button-div').classList.remove('hidden');
 	document.querySelector('#mobile-menu').classList.add('hidden');
 }
+
+function submitContactForm(form){
+	event.preventDefault();
+	var submitButton = document.getElementById('contact-submit');
+	submitButton.disabled = true;
+	submitButton.classList.remove('clickable');
+	emailjs.sendForm('service_vhw7slg', 'template_q4j4dpi', form)
+	.then(function(){
+		document.getElementById('contact-submit-success').classList.remove('hidden');
+	}, function(error){
+		document.getElementById('contact-submit-error').classList.remove('hidden');
+	});
+}
