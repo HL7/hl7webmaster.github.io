078a41d603576119c4161fbf8403d2ec2f1ebcb3
Dylan Phelan <dtphelan1@gmail.com>
2022-12-08 16:31:21 -0500

    fixed contact us padding

--- pages/contact.vue
+++ pages/contact.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    class="px-8 pt-8 md:pt-28 lg:pt-64 pb-6 bg-darkgrey text-white flex flex-col !justify-center md:flex-nowrap md:flex-row md:gap-8 lg:gap-16 xl:gap-32"
+    class="mx-8 mt-11 md:mt-[7.75rem] mb-6 bg-darkgrey text-white flex flex-col !justify-center md:flex-nowrap md:flex-row md:gap-8 lg:gap-16 xl:gap-32"
   >
     <div
       class="flex flex-col justify-center items-center md:justify-start md:items-start md:w-1/2 xl:w-1/4"
