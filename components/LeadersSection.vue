4498387655194de5487d561f2bd91ae32916da36
Dylan Phelan <dtphelan1@gmail.com>
2023-01-26 15:48:23 -0500

    added suzette, restored layout of leaders to original proportions

--- components/LeadersSection.vue
+++ components/LeadersSection.vue
@@ -3,14 +3,11 @@
     <h1 tabindex="2" class="satoshi-black font-bold font-36px text-darkbrown">
       Leadership
     </h1>
-    <div
-      id="leaderApp"
-      class="pt-2.5 grid grid-cols-1 sm:grid-cols-2 sm:gap-x-4 lg:grid-cols-4 lg:gap-x-11 justify-between"
-    >
-      <ul
+    <ul id="leaderApp" class="pt-2.5 flex flex-wrap justify-center pb-4">
+      <li
         v-for="leader in leaders"
         :key="leader['title']"
-        class="w-full pb-4 flex flex-col"
+        class="w-full flex flex-col basis-full lg:basis-1/5 p-2"
       >
         <img
           :title="leader['image-alt']"
@@ -28,8 +25,8 @@
         <h3 tabindex="2" class="font-medium text-medgrey">
           {{ leader['title'] }}
         </h3>
-      </ul>
-    </div>
+      </li>
+    </ul>
   </div>
 </template>
 <script>
