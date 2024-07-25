8e785a8f62cfcf73557219230390413922d79625
Dylan Mendelowitz <dmendelowitz@mitre.org>
2024-07-25 15:03:50 -0400

    lint fixes

--- layouts/DefaultGrey.vue
+++ layouts/DefaultGrey.vue
@@ -1,8 +1,13 @@
 <template>
-  <div class="h-full min-w-full container flex flex-col justify-between sans bg-darkgrey">
+  <div
+    class="h-full min-w-full container flex flex-col justify-between sans bg-darkgrey"
+  >
     <Header
-      logo-color="white" mobile-text-color="white" mobile-logo-color="white"
-      mobile-class="mobile-header-container-grey " />
+      logo-color="white"
+      mobile-text-color="white"
+      mobile-logo-color="white"
+      mobile-class="mobile-header-container-grey "
+    />
     <Nuxt />
     <Footer />
   </div>
--- pages/contact.vue
+++ pages/contact.vue
@@ -1,14 +1,21 @@
 <template>
   <div
-    class="mx-8 mt-11 md:mt-[7.75rem] mb-6 bg-darkgrey text-white flex flex-col !justify-center md:flex-nowrap md:flex-row md:gap-8 lg:gap-16 xl:gap-32">
-    <div class="flex flex-col justify-center items-center md:justify-start md:items-start md:w-1/2 xl:w-1/4">
+    class="mx-8 mt-11 md:mt-[7.75rem] mb-6 bg-darkgrey text-white flex flex-col !justify-center md:flex-nowrap md:flex-row md:gap-8 lg:gap-16 xl:gap-32"
+  >
+    <div
+      class="flex flex-col justify-center items-center md:justify-start md:items-start md:w-1/2 xl:w-1/4"
+    >
       <div class="self-start">
         <h2 class="satoshi-black font-bold contact-heading pb-2">
           Let's Connect!
         </h2>
       </div>
       <div class="flex justify-center py-8 max-w-sm md:hidden">
-        <img class="max-w-l" alt="Email and letter illustration" src="~/assets/img/letter.png" />
+        <img
+          class="max-w-l"
+          alt="Email and letter illustration"
+          src="~/assets/img/letter.png"
+        />
       </div>
       <div class="w-full">
         <p class="py-2 font-normal">
@@ -24,11 +31,14 @@
         </p>
       </div>
     </div>
-    <div v-once id="hubspotForm" class="bg-white px-8 py-2 rounded-lg w-full md:w-1/2 mt-6 md:mt-0 xl:w-1/3"></div>
+    <div
+      v-once
+      id="hubspotForm"
+      class="bg-white px-8 py-2 rounded-lg w-full md:w-1/2 mt-6 md:mt-0 xl:w-1/3"
+    ></div>
   </div>
 </template>
 <script>
-
 export default {
   layout: 'DefaultGrey',
   head() {
@@ -47,15 +57,15 @@ export default {
   },
   mounted() {
     const contactFormScript = document.createElement('script')
-    contactFormScript.src = "https://js.hsforms.net/forms/v2.js";
-    document.body.appendChild(contactFormScript);
-    contactFormScript.addEventListener("load", () => {
+    contactFormScript.src = 'https://js.hsforms.net/forms/v2.js'
+    document.body.appendChild(contactFormScript)
+    contactFormScript.addEventListener('load', () => {
       if (window.hbspt) {
         window.hbspt.forms.create({
-          region: "na1",
-          portalId: "8047841",
-          formId: "9f72a8ab-5458-49c6-a88b-f27688d23792",
-          target: "#hubspotForm"
+          region: 'na1',
+          portalId: '8047841',
+          formId: '9f72a8ab-5458-49c6-a88b-f27688d23792',
+          target: '#hubspotForm',
         })
       }
     })
